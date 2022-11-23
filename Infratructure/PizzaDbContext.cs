24d5dcfa7b03278e64a0da1184c27ba5aa6d8e42
AndersJakobsen <ande12n3@easv365.dk>
2022-11-23 08:36:01 +0100

    db changes

--- Infratructure/PizzaDbContext.cs
+++ Infratructure/PizzaDbContext.cs
@@ -1,8 +1,21 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure;
 
 public class PizzaDbContext : DbContext
 {
+    public PizzaDbContext(DbContextOptions<PizzaDbContext> opts) : base(opts)
+    {
+        
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {   
+        modelBuilder.Entity<Pizza>()
+            .Property(p => p.Id)
+            .ValueGeneratedOnAdd();
+    }
     
+    public DbSet<Pizza> PizzaTable { get; set; }
 }
--- Infratructure/PizzaRepository.cs
+++ Infratructure/PizzaRepository.cs
@@ -7,6 +7,18 @@ namespace Infrastructure;
 
 public class PizzaRepository : IPizzaRepository
 {
+   
+    private PizzaDbContext _pizzaContext;
+
+    public PizzaRepository(PizzaDbContext context)
+    {
+        _pizzaContext = context;
+    }
+    public void RebuildDB()
+    {
+        _pizzaContext.Database.EnsureDeleted();
+        _pizzaContext.Database.EnsureCreated();
+    }
     public List<Pizza> GetAll()
     {
         throw new NotImplementedException();
